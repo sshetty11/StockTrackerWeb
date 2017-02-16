1a1695a562bc8df0646eeec095e335f1070c616d
sshetty11 <shilparshetty11@gmail.com>
2017-02-16 09:23:24 -0800

    Added error handling, improved code

--- app/Services.js
+++ app/Services.js
@@ -3,7 +3,7 @@ angular.module('StockTrackerApp.services', [])
   function($rootScope, $http, $q, $log) {
     var stockAPI = {};
     var env = 'http://localhost:9000'
-      stockAPI.getStockResponse = function(search_param) {
+    stockAPI.getStockResponse = function(search_param) {
       id_arr = search_param.split(",");
       id = id_arr[id_arr.length - 1];
       return $http({
@@ -12,12 +12,12 @@ angular.module('StockTrackerApp.services', [])
       });
     }
 
-      stockAPI.autoComplete = function(txtValue){
+    stockAPI.autoComplete = function(txtValue) {
       return $http({
         method: 'JSONP',
-        url: env + '/stock/autocomplete/'+txtValue +'?callback=JSON_CALLBACK'
+        url: env + '/stock/autocomplete/'+ txtValue +'?callback=JSON_CALLBACK'
       });
     }
-    return   stockAPI;
+    return stockAPI;
   }
 ]);
--- app/controllers.js
+++ app/controllers.js
@@ -1,16 +1,20 @@
 angular.module('StockTrackerApp.controllers', [ 'ngMaterial' ]).
-  controller('driversController', function($scope, $http, $log, $q, stockAPIservice) {
+  controller('stockController', function($scope, $http, $log, $q, stockAPIservice) {
     $scope.nameFilter = null;
-    $scope.listVisibility = false;
-    $scope.divVisibility = false;
-
     $scope.update = function(stockname) {
       $scope.stockname = stockname;
+      $scope.loading = true;
+      $scope.listVisibility = false;
+      $scope.divVisibility = false;
       stockAPIservice.getStockResponse($scope.stockname).success(function (response) {
-        //Dig into the responde to get the relevant data
         $scope.stockResponse = response;
         $scope.listVisibility = true;
         $scope.divVisibility = true;
+        $scope.loading = false;
+        $scope.error = false;
+      }).error(function (data, status) {
+        $scope.error = true;
+        $scope.loading = false;
       });
     };
     var self = this;
@@ -21,8 +25,14 @@ angular.module('StockTrackerApp.controllers', [ 'ngMaterial' ]).
     function querySearch (query) {
       deferred = $q.defer();
       stockAPIservice.autoComplete(query).success(function (response) {
+        $scope.error = false;
         deferred.resolve(response);
-      });
+      }).error(function (data, status) {
+        $scope.error = true;
+        $scope.loading = false;
+        $scope.listVisibility = false;
+        $scope.divVisibility = false;
+      });;
       return deferred.promise;
     }
 });
